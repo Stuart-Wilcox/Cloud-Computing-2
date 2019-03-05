4bfa9f1a70820a9fb1988b55945a76248e8c92b4
Gautham Padmakumar <gpadmaku@gmail.com>
2019-03-04 22:39:33 -0500

    finished upgrade, delete, downgrade vm

--- application/src/app/create-vm/create-vm.component.ts
+++ application/src/app/create-vm/create-vm.component.ts
@@ -4,6 +4,7 @@ import {MatSnackBar} from '@angular/material';
 
 import { VM } from '@models/VM';
 import { VMService } from '@services/vm.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-vm',
@@ -17,7 +18,7 @@ export class CreateVmComponent implements OnInit {
   public type: string;
   private vms: VM[];
 
-  constructor(public vmService: VMService, private snackBar: MatSnackBar) {
+  constructor(private router: Router, public vmService: VMService, private snackBar: MatSnackBar) {
     this.vms = [];
     this.error = null;
   }
@@ -35,15 +36,16 @@ export class CreateVmComponent implements OnInit {
       this.loading = true;
       this.vmService.createVM(this.name, this.type).then(vm => {
         this.loading = false;
-        this.snackBar.open("VM Created Successfully.", '', {duration: 500});
+        this.snackBar.open("VM Created Successfully.", '', {duration: 1000});
         this.name = null;
         this.type = null;
+        this.router.navigate(['/dashboard']);
       }).catch(err => {
         this.loading = false;
         this.error = err;
       });
     } else {
-      this.snackBar.open("Ensure all the fields are filled.", '', {duration: 500});
+      this.snackBar.open("Ensure all the fields are filled.", '', {duration: 1000});
     }
   }
 }
--- application/src/app/view-vm/view-vm.component.ts
+++ application/src/app/view-vm/view-vm.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatButtonModule } from '@angular/material/button';
+import {MatSnackBar} from '@angular/material';
+import { Router } from '@angular/router';
 
 import { VMService } from '@services/vm.service';
 import { VM } from '@models/VM';
@@ -16,7 +18,7 @@ export class ViewVmComponent implements OnInit {
   public id: string;
   public loading: boolean;
 
-  constructor(private route: ActivatedRoute, public vmService: VMService) {
+  constructor(private route: ActivatedRoute, public vmService: VMService, private snackBar: MatSnackBar, private router: Router) {
     this.id = '';
     this.vm = null;
   }
@@ -29,6 +31,33 @@ export class ViewVmComponent implements OnInit {
         this.vm = vm;
         this.loading = false;
       });
+    });
+  }
+
+  deleteVM() {
+    this.vmService.deleteVM(this.id).then((res) => {
+      this.snackBar.open("VM Deleted Successfully.", '', {duration: 1000});
+      this.router.navigate(['/dashboard']);
+    }).catch(err => {
+      this.snackBar.open("Error. VM Deletion Failed.", '', {duration: 1000});
+    });
+  }
+
+  upgradeVM(){
+    this.vmService.upgradeVM(this.id).then((vm) => {
+      this.snackBar.open("VM Upgraded Successfully.", '', {duration: 1000});
+      this.vm = vm;
+    }).catch(err => {
+      this.snackBar.open("Error. VM Upgrade Failed.", '', {duration: 1000});
+    });
+  }
+
+  downgradeVM(){
+    this.vmService.downgradeVM(this.id).then((vm) => {
+      this.snackBar.open("VM Downgraded Successfully.", '', {duration: 1000});
+      this.vm = vm;
+    }).catch(err => {
+      this.snackBar.open("Error. VM Downgrade Failed.", '', {duration: 1000});
     });
   }
 }
