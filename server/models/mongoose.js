70cd775ae1022e70e67e364fca0959c1521081a8
unknown <agoston.the.dev@gmail.com>
2019-03-31 01:45:35 -0400

    Fix db connection string

--- server/models/mongoose.js
+++ server/models/mongoose.js
@@ -5,7 +5,7 @@ const user = process.env.DB_USER;
 
 // connection string => mongodb+srv://stu:<PASSWORD>@cluster0-83uwo.mongodb.net/test?retryWrites=true
 // mongodb://stu:<PASSWORD>@cluster0-shard-00-00-83uwo.mongodb.net:27017,cluster0-shard-00-01-83uwo.mongodb.net:27017,cluster0-shard-00-02-83uwo.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true
-let connectionString = `mongodb://${user}:${password}@${host}/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`;
+let connectionString = `mongodb://${user}:${password}@${host}/test`;
 mongoose.connect(connectionString, { useNewUrlParser: true });
 
 module.exports = mongoose;
